e657b24e78cf6ce09b0b75546097f7ae5caca15a
Manar Jber <mohamadmanar960@gmail.com>
2024-01-09 20:04:09 +0200

    ed

--- grad_proj/lib/usrTime/screens/detailstore1/store1/componants/Brand.dart
+++ grad_proj/lib/usrTime/screens/detailstore1/store1/componants/Brand.dart
@@ -27,7 +27,7 @@ class _BrandviweState extends State<Brandviwe> {
 
 Future<void> fetchProductsForStore(String name) async {
     var response = await http.get(
-      Uri.parse('http://localhost:4000/brand-product/brand/$name'),
+      Uri.parse('http://localhost:4000/brand-product/cat/$name/brand'),
       headers: {"Content-Type": "application/json"},
     );
 
@@ -56,31 +56,34 @@ Future<void> fetchProductsForStore(String name) async {
         ),
            Padding(
              padding: const EdgeInsets.all(30),
-             child: GridView.builder(
-                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: Responsive.isDesktop(context)?3:2, 
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-              childAspectRatio: Responsive.isDesktop(context)?2:1, 
-                       ),
-                       itemCount: item?.length ?? 0,
-                       shrinkWrap: true,
-                       physics: const NeverScrollableScrollPhysics(),
-                       itemBuilder: (context, index) {
-                     return  CatCard(
-                    name: item![index]['categoryName'],
-                    image: item![index]['categoryImage'],
-                    press: () {
-                            Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Productsub(item: item![index]['products']),
-                      ),
+             child: Directionality(
+              textDirection: TextDirection.rtl,
+               child: GridView.builder(
+                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: Responsive.isDesktop(context)?3:2, 
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                childAspectRatio: Responsive.isDesktop(context)?2:1, 
+                         ),
+                         itemCount: item?.length ?? 0,
+                         shrinkWrap: true,
+                         physics: const NeverScrollableScrollPhysics(),
+                         itemBuilder: (context, index) {
+                       return  CatCard(
+                      name: item![index]['categoryName'],
+                      image: item![index]['categoryImage'],
+                      press: () {
+                              Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Productsub(item: item![index]['products']),
+                        ),
+                      );
+                      },
                     );
-                    },
-                  );
-                       },
-                     ),
+                         },
+                       ),
+             ),
            ),
       ],
     );
